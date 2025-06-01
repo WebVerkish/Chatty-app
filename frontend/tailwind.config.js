6983c90ab791a26d278392a7fcd4f9d0f811776a
Rahul Kishne <kishnerahul123@gmail.com>
2025-06-01 21:10:07 +0530

    some changes dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,9 @@
-import daisyui from "daisyui";
-
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: [
       "light",
