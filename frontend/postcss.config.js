c46400a65b1eec6f2a90fb47c6de6909869621e8
Rahul Kishne <kishnerahul123@gmail.com>
2025-06-01 21:14:22 +0530

    some changes dependencies

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,4 +1,7 @@
-export default  {
+import tailwindcss from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
