3d67e6ccefeda06da2d1e2c2c2cb9e76468e9455
Rahul Kishne <kishnerahul123@gmail.com>
2025-06-01 21:17:26 +0530

    some changes dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import tailwindcss from "@tailwindcss/vite";
+import daisyui from "daisyui";
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss({
+      config: {
+        plugins: [daisyui],
+        daisyui: {
+          themes: [
+            "light",
+            "dark",
+            "cupcake",
+            "bumblebee",
+            "emerald",
+            "corporate",
+            "synthwave",
+            "retro",
+            "cyberpunk",
+            "valentine",
+            "halloween",
+            "garden",
+            "forest",
+            "aqua",
+            "lofi",
+            "pastel",
+            "fantasy",
+            "wireframe",
+            "black",
+            "luxury",
+            "dracula",
+            "cmyk",
+            "autumn",
+            "business",
+            "acid",
+            "lemonade",
+            "night",
+            "coffee",
+            "winter",
+            "dim",
+            "nord",
+            "sunset",
+          ],
+        },
+      },
+    }),
+  ],
   build: {
-    outDir: "dist",
+    outDir: "dist"
   },
   server: {
     proxy: {
